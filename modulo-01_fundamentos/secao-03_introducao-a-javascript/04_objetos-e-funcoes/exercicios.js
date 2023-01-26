1444ecaea0c02c54829229cac29f3058f5c47040
Victor Salis <vsalis@live.com>
2023-01-26 18:29:30 -0300

    Fazendo exercícios

--- modulo-01_fundamentos/secao-03_introducao-a-javascript/04_objetos-e-funcoes/exercicios.js
+++ modulo-01_fundamentos/secao-03_introducao-a-javascript/04_objetos-e-funcoes/exercicios.js
@@ -158,3 +158,88 @@ console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${play
         return (!isNaN(parseFloat(x)) && isFinite(x));
     }
 }
+{
+    let info = {
+        personagem: 'Margarida',
+        origem: 'Pato Donald',
+        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+    //Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console: 
+    console.log(`Bem vinda, ${info.personagem}`);
+
+    //Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console: 
+    info["recorrente"] = "sim";
+    console.log(info);
+
+    //Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console: 
+
+    for (let key in info){
+        console.log(key);
+    }
+
+    // Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console: 
+
+    for (let key in info){
+        console.log(info[key]);
+    }
+
+    //Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console: 
+
+    let info2 = {
+        personagem: 'Tio Patinhas',
+        origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+        nota: 'O último MacPatinhas',
+        recorrente: "sim"
+    };
+
+    for (let key in info){
+        if(key == "recorrente"){
+            if(info[key] == info2[key]){
+                console.log("Ambos recorrentes");
+                continue;
+            }
+        }
+        console.log(`${info[key]} e ${info2[key]}`);
+    }
+}
+
+{
+    let leitor = {
+        nome: 'Julia',
+        sobrenome: 'Pessoa',
+        idade: 21,
+        livrosFavoritos: [
+          {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+          },
+        ],
+      };
+
+
+
+    // Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+    // Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto contendo as seguintes informações:
+
+    console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`);
+
+    leitor.livrosFavoritos.push(
+        {
+
+            titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+          
+            autor: 'JK Rowling',
+          
+            editora: 'Rocco',
+          
+          }
+    );
+
+    // Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código. 
+
+    console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`);
+
+}
