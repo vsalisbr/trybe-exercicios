25c432aeb9b977e699055f0edcb30a5bbb9b2114
Victor Salis <vsalis@live.com>
2023-01-28 13:36:50 -0300

    Fazendo execício bônus

--- modulo-01_fundamentos/secao-03_introducao-a-javascript/03_logica-de-programacao-e-algoritmos/exercicios-bonus.js
+++ modulo-01_fundamentos/secao-03_introducao-a-javascript/03_logica-de-programacao-e-algoritmos/exercicios-bonus.js
@@ -47,7 +47,7 @@
 {
     //Depois, faça uma pirâmide com n asteriscos de base: 
 
-    let qtdLinhas = Math.floor((Math.random() * 15))+1;;
+    let qtdLinhas = Math.floor((Math.random() * 20))+5;;
     if (qtdLinhas%2==0){
         qtdLinhas -= 1;
     }
@@ -72,5 +72,56 @@
 
 {
     //Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar: 
+    let qtdLinhas = Math.floor((Math.random() * 30))+10;;
+    if (qtdLinhas%2==0){
+        qtdLinhas -= 1;
+    }
+    for (let i=1;i<=qtdLinhas;++i){
+        let impressao = "";
+        let qtdEspacosBrancos = qtdLinhas-i;
+        if(qtdEspacosBrancos%2!==0){
+            continue;
+        }
+        for (let y=1;y<=(qtdEspacosBrancos/2);++y){
+            impressao += " ";
+        }
+        for (let z=1;z<=i;++z){
+            if (i==1) {
+                impressao += "*";
+                continue;
+            }
+            if (i==qtdLinhas) {
+                impressao += "*";
+            }
+            if (i !== 1 && i !== qtdLinhas) {
+                if (z == 1 || z == i) {
+                    impressao += "*";
+                } else {
+                    impressao += ' ';
+                }
+            }
+            
+        }
+        for (let w=1;w<=(qtdEspacosBrancos/2);++w){
+            impressao += " ";
+        }
+        console.log(impressao);      
+    }
+}
 
+{
+    // Faça um programa que diz se um número definido numa variável é primo ou não.
+
+    // Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele por quaisquer outros números dá resto diferente de zero.
+
+    const ePrimo = (number) => {
+        for (let i = 2; i < number; i += 1) {
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    console.log(ePrimo(7));
 }
