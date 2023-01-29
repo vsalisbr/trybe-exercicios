3cc307c318d6df3f6c4bf486122e0c185a46df96
Victor Salis <vsalis@live.com>
2023-01-29 11:21:59 -0300

    Implemento ESLINT no repositório

--- modulo-01_fundamentos/secao-03_introducao-a-javascript/05_js-es6-let-const-arrowfunctions-templateliterals/exercicios.js
+++ modulo-01_fundamentos/secao-03_introducao-a-javascript/05_js-es6-let-const-arrowfunctions-templateliterals/exercicios.js
@@ -1,17 +1,17 @@
-{
-// Altere o tipo das variáveis (var) para let ou const para que respeitem o escopo em que foram declaradas.
+{ 
+  // Altere o tipo das variáveis (var) para let ou const para que respeitem o escopo em que foram declaradas.
 
-// 🚀 Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+  // 🚀 Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
 
-// Copie o código abaixo e rode-o para verificar sua saída:
+  // Copie o código abaixo e rode-o para verificar sua saída:
 
-
-function imprimeIdade() {
+  // eslint-disable-next-line no-inner-declarations
+  function imprimeIdade() {
     for (let idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
+      console.log('Idade dentro do for:', idade);
     }
-  }
-  imprimeIdade()
+  }  
+  imprimeIdade();
 
 }
 
@@ -20,7 +20,7 @@ function imprimeIdade() {
   const pessoa = {
     nome: 'Henri',
     idade: 20
-  }
+  };
 
   pessoa.nome = 'Luna';
   pessoa.idade = 19;
@@ -30,50 +30,51 @@ function imprimeIdade() {
 }
 
 {
-    // Modifique a variável para que não ocorra Erro; 
-    let favoriteFood = 'Lasanha';
-    favoriteFood = 'Hamburguer';
-    console.log(favoriteFood);
+  // Modifique a variável para que não ocorra Erro; 
+  let favoriteFood = 'Lasanha';
+  favoriteFood = 'Hamburguer';
+  console.log(favoriteFood);
 }
 
 {
-    //Modifique as concatenações para template literals.
-    const name = 'Adriana';
-    const lastName = 'Soares';
-    console.log(`Olá ${name} ${lastName}!`);
-    function soma(a,b) {
-      let resultado = a + b;
-      return resultado;
-    }
-    let a = 3;
-    let b = 5;
-    console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+  //Modifique as concatenações para template literals.
+  const name = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Olá ${name} ${lastName}!`);
+  // eslint-disable-next-line no-inner-declarations
+  function soma(a, b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a, b)}`);
 }
 
 {
-    // Transforme a função numeroAleatorio em uma arrow function;
-    const numeroAleatorio = () => Math.random();
-    console.log(numeroAleatorio());
+  // Transforme a função numeroAleatorio em uma arrow function;
+  const numeroAleatorio = () => Math.random();
+  console.log(numeroAleatorio());
 }
 
 {
-    //Transforme a função hello em uma arrow function; 
-    const hello = (nome) => `Olá, ${nome}!`;
-    let nome = 'Ivan';
-    console.log(hello(nome));
+  //Transforme a função hello em uma arrow function; 
+  const hello = (nome) => `Olá, ${nome}!`;
+  let nome = 'Ivan';
+  console.log(hello(nome));
 }
 
 {
-    //Transforme a função nomeCompleto em uma arrow function; 
-    const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
-    let nome = 'Ivan';
-    let sobrenome = 'Pires';
-    console.log(nomeCompleto(nome, sobrenome));
+  //Transforme a função nomeCompleto em uma arrow function; 
+  const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome));
 }
 
 {
-    //Altere a expressão if/else utilizando ternary operator;
-    let speed = 90;
-    const speedCar = (speed) => speed >= 120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
-    console.log(speedCar(speed));
-}
+  //Altere a expressão if/else utilizando ternary operator;
+  let speed = 90;
+  const speedCar =(speed) => speed >= 120 ? 'Você ultrapassou o limite de velocidade' : 'Você está na velocidade permitida';
+  console.log(speedCar(speed));
+} 
