27218d577053da505e3988aa51bdf9ec97ea0cf3
Victor Salis <vsalis@live.com>
2023-02-05 09:36:12 -0300

    Retomando exercício 4.4

--- modulo-01_fundamentos/secao-04_javascript-dom-eventos-e-webstorage/04_web-storage/js/scripts.js
+++ modulo-01_fundamentos/secao-04_javascript-dom-eventos-e-webstorage/04_web-storage/js/scripts.js
@@ -1,63 +1,223 @@
-const dataTbl = {
-  header: [
-    {
-      text: 'Cor de Fundo'
-    },
-    {
-      text: 'Tamanho da fonte'
-    },
-    {
-      text: 'Espaçamento entre as linhas'
-    }
-  ]
+window.onload = () => {
+  if (!localStorage.cfg) {
+    setCfg();
+  } else {
+    getCfg();
+  }
+  applyCfg();
 };
-const btns = [
+
+let cfgObj = {
+  cfgVersion: 0.3,
+  backgroundColor: 'white',
+  fontColor: 'black',
+  fontSize: '1pt',
+  lineSize: '1px',
+  font: 'Arial',
+};
+
+const dataTbl = [
   {
-    text: 'white',
-    section: 'corDeFundo',
+    header: 'Cor de Fundo',
+    onClick: setBackgroundColor,
+    data: [
+      {
+        text: 'white',
+      },
+      {
+        text: 'black',
+      },
+      {
+        text: 'green',
+      },
+      {
+        text: 'blue',
+      },
+      {
+        text: 'yellow',
+      },
+    ]
   },
   {
-    text: 'black',
-    section: 'corDeFundo',
+    header: 'Cor da fonte',
+    onClick: setFontColor,
+    data: [
+      {
+        text: 'black'
+      },
+      {
+        text: 'red'
+      },
+      {
+        text: 'white'
+      }
+    ]
   },
   {
-    text: 'green',
-    section: 'corDeFundo',
+    header: 'Tamanho da fonte',
+    onClick: setFontSize,
+    data: [
+      {
+        text: '8pt'
+      },
+      {
+        text: '10pt'
+      },
+      {
+        text: '12pt'
+      },
+      {
+        text: '14pt'
+      },
+      {
+        text: '20pt'
+      }
+    ]
   },
   {
-    text: 'blue',
-    section: 'corDeFundo',
+    header: 'Espaçamento entre as linhas',
+    onClick: setLineSize,
+    data: [
+      {
+        text: '1.0'
+      },
+      {
+        text: 'normal'
+      },
+      {
+        text: '1.5'
+      },
+      {
+        text: '2.0'
+      },
+      {
+        text: '3.0'
+      }
+    ]
   },
   {
-    text: 'yellow',
-    section: 'corDeFundo',
-  },
+    header: 'Tipo da fonte',
+    onClick: setFont,
+    data: [
+      {
+        text: 'Arial'
+      },
+      {
+        text: 'Times New Roman'
+      }
+    ]
+  }
 ];
 
 const creatTbl = () => {
   const btnsArea = document.querySelector('#btnsArea');
   const table = newTbl();
   table.appendChild(tblHeader());
+  const tblD = tblData();
+  for (let i = 0; i < tblD.length; i += 1) {
+    table.appendChild(tblD[i]);
+  }
   btnsArea.appendChild(table);
 };
 
 const tblHeader = () => {
   const tblRow = newTblRow();
-  for (key of dataTbl.header) {
-    let tblH = newTblH();
-    tblH.innerHTML = key.text;
+  for (let i = 0; i < dataTbl.length; i += 1) {
+    const tblH = newTblH();
+    tblH.innerHTML = dataTbl[i].header;
     tblRow.appendChild(tblH);
   }
   return tblRow;
 };
 
-const tblD = () => {
-  const tblD = newTblD();
+const tblRows = () => {
+  const rows = [[], 0];
+  for (let i = 0; i < dataTbl.length; i += 1) {
+    const data = dataTbl[i].data;
+    rows[1] = data.length > rows[1] ? data.length : rows[1];
+  }
+  for (let i = 0; i < rows[1]; i += 1) {
+    const tblRow = newTblRow();
+    rows[0].push(tblRow);
+  }
+  return rows;
+};
+
+const tblData = () => {
+  const arrayRows = tblRows();
+  const rows = arrayRows[0];
+  let numberOfLines = arrayRows[1];
+  for (let i = 0; i < dataTbl.length; i += 1) {
+    const data = dataTbl[i].data;
+    for (let y = 0; y < numberOfLines; y+= 1) {
+      const tblRow = rows[y];
+      const tblD = newTblD();      
+      if (data[y]){
+        const btn = newBtn();
+        btn.innerHTML = data[y].text;
+        if (dataTbl[i].onClick) {
+          btn.addEventListener('click', dataTbl[i].onClick);
+        }        
+        tblD.appendChild(btn);
+      } else {
+        tblD.innerHTML = '';
+      }      
+      tblRow.appendChild(tblD);
+      rows[y] = tblRow;
+    }
+  }
+  return rows;
 };
 
 const newTbl = () => document.createElement('table');
 const newTblRow = () => document.createElement('tr');
 const newTblH = () => document.createElement('th');
 const newTblD = () => document.createElement('td');
+const newBtn = () => document.createElement('button');
 
 creatTbl();
+
+function setBackgroundColor (event) {
+  cfgObj.backgroundColor = event.target.textContent;
+  setCfg();
+}
+
+function setFontColor (event) {
+  cfgObj.fontColor = event.target.textContent;
+  setCfg();
+}
+
+function setFontSize (event) {
+  cfgObj.fontSize = event.target.textContent;
+  setCfg();
+}
+
+function setLineSize (event) {
+  cfgObj.lineSize = event.target.textContent;
+  setCfg();
+}
+
+function setFont(event) {
+  cfgObj.font = event.target.textContent;
+  setCfg();
+}
+
+function setCfg() {
+  localStorage.cfg = JSON.stringify(cfgObj);
+  applyCfg();
+}
+
+function getCfg() {
+  const cfgLocalStorage = JSON.parse(localStorage.cfg);
+  if (parseFloat(cfgLocalStorage.cfgVersion) < parseFloat(cfgObj.cfgVersion)) {
+    localStorage.clear();
+    location.reload();
+  }
+  cfgObj = cfgLocalStorage;
+}
+
+function applyCfg() {
+  const body = document.getElementById('body');
+  body.style.backgroundColor = cfgObj.backgroundColor;
+  console.log(cfgObj);
+}
